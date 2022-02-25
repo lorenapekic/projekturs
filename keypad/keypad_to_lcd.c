9726c11530eea2cf21e571fab3d168f394ad97c4
Lorena Pekic <lpekic@riteh.hr>
2022-02-26 00:41:10 +0100

    Izmijenjena funkcija pretvorbe binarnih brojeva u dekadske i dodan UART

--- keypad/keypad_to_lcd.c
+++ keypad/keypad_to_lcd.c
@@ -15,7 +15,7 @@ KeyPad pin layout:
 	
 ###############################################################################
 	
-lcd.h on PORTD
+lcd.h: data pins on PB0-PB3, config pins on PD4-PD7
 
 ###############################################################################
 
@@ -36,7 +36,7 @@ lcd.h on PORTD
 # --> 1111 0000
 */
 
-#define F_CPU 8000000UL
+#define F_CPU 7372800UL
 
 #define DDRx DDRA
 #define PORTx PORTA
@@ -49,7 +49,11 @@ lcd.h on PORTD
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <avr/interrupt.h>
 
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+volatile char REC = 0xFF;
 
 
 uint8_t digit[4][3] = {
@@ -59,11 +63,16 @@ uint8_t digit[4][3] = {
 	{0x0F, 0, 0xF0}
 };
 
-// iskoristen kod s interneta samo za prikaz na LCD
-char convert(uint8_t *a) {
-	if (*a == 0x0F) return '*';
-	else if (*a == 0xF0) return '#';
-	else return *a + '0';
+char convert(uint8_t a) {
+	switch (a) {
+		case 0x0F:
+			return '*';
+		case 0xF0:
+			return '#';
+		default:
+			return a + '0';
+	}
+	return a + '0';
 }
 
 void Col_init() {
@@ -83,27 +92,61 @@ void Read_key() {
 	uint8_t col = 0xFF;
 
 	Col_init();
-	if (bit_is_clear(PINx, 0)) row = 0x00;
-	else if (bit_is_clear(PINx, 1)) row = 0x01;
-	else if (bit_is_clear(PINx, 2)) row = 0x02;
+	if (bit_is_clear(PINx, 0)) col = 0x00;
+	else if (bit_is_clear(PINx, 1)) col = 0x01;
+	else if (bit_is_clear(PINx, 2)) col = 0x02;
 
 	Row_init();
-	if (bit_is_clear(PINx, 3)) col = 0x00;
-	else if (bit_is_clear(PINx, 4)) col = 0x01;
-	else if (bit_is_clear(PINx, 5)) col = 0x02;
-	else if (bit_is_clear(PINx, 6)) col = 0x03;
+	if (bit_is_clear(PINx, 3)) row = 0x00;
+	else if (bit_is_clear(PINx, 4)) row = 0x01;
+	else if (bit_is_clear(PINx, 5)) row = 0x02;
+	else if (bit_is_clear(PINx, 6)) row = 0x03;
 
 	lcd_clrscr();
-	lcd_puts(convert(&digit[col][row]));
+	lcd_putc(convert(digit[row][col]));
 	
 	_delay_ms(100);
 }
 
+void USART_Initialization(unsigned int BAUD) {
+	UBRRL = BAUD_PRESCALE;
+	UBRRH = (BAUD_PRESCALE >> 8);
+	UCSRB |= (1 << RXEN) | (1 << RXCIE);
+	UCSRC |= (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1);
+}
+
+ISR(USART_RXC_vect) {
+	REC = UDR;
+}
+
+void card_reader_teseting() {
+	if ((UCSRA & (1 << FE)))
+	{
+		lcd_putc('F');
+	}
+	if ((UCSRA & (1 << DOR)))
+	{
+		lcd_putc('D');
+	}
+	if ((UCSRA & (1 << UPE)))
+	{
+		lcd_putc('P');
+	}
+	if (REC != 0xFF)
+	{
+		lcd_putc('6');
+		REC = 0xFF;
+	}
+}
+
 void init_lcd() {
-	DDRD = _BV(4);
+	DDRD = _BV(4) | _BV(1);
 	TCCR1A = _BV(COM1B1) | _BV(WGM10);
 	TCCR1B = _BV(WGM12) | _BV(CS11);
 	OCR1B = 128;
+
+	USART_Initialization(USART_BAUDRATE);
+	sei();
 
 	lcd_init(LCD_DISP_ON);
 	lcd_clrscr();
@@ -111,9 +154,13 @@ void init_lcd() {
 
 int main() {
 	init_lcd();
-
+	
 	while(1) {
 		Col_init();
-		if(bit_is_clear(PINx, 0) ^ bit_is_clear(PINx, 1) ^ bit_is_clear(PINx, 2)) Read_key();
+		if(bit_is_clear(PINx, 0) ^ bit_is_clear(PINx, 1) ^ bit_is_clear(PINx, 2)) {
+			Read_key();
+		}
+		
+		card_reader_teseting();
 	}
 }
